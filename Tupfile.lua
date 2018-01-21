4b4af623e38e56bdb095693bf0da3ca1a63a404d
Samuel Sadok <samuel.sadok@bluewin.ch>
2018-01-22 00:45:16 +0100

    add rpi_ws281x library

--- Tupfile.lua
+++ Tupfile.lua
@@ -1,9 +1,20 @@
 
-SOURCES = {'lightd.cpp','fibre/cpp/posix_udp.cpp','fibre/cpp/protocol.cpp'}
+CPP_SOURCES = {'lightd.cpp','fibre/cpp/posix_udp.cpp','fibre/cpp/protocol.cpp'}
 INCLUDE = {'fibre/cpp/include'}
-LIBS = {'pthread'}
-CFLAGS = {'-std=c++11'}
+LIBS = {'pthread', 'rt'}
+CFLAGS = {}
 LDFLAGS = {}
+
+-- C sources
+C_SOURCES = {
+    'rpi_ws281x/mailbox.c',
+    'rpi_ws281x/ws2811.c',
+    'rpi_ws281x/pwm.c',
+    'rpi_ws281x/pcm.c',
+    'rpi_ws281x/dma.c',
+    'rpi_ws281x/rpihw.c'
+}
+
 
 -- to cross compile:
 --CFLAGS += '-target armv6l-unknown-linux-gnueabihf --sysroot=/usr/arm-linux-gnueabihf'
@@ -18,26 +29,38 @@ for _,lib in pairs(LIBS) do
     LDFLAGS += "-l"..lib
 end
 
--- Create compile rules
-OBJECTS = {}
-for _,src in pairs(SOURCES) do
-    obj="build/"..src:gsub("/","_")..".o"
-    tup.frule{
-        inputs=src,
-        command='arm-linux-gnueabihf-g++ -c %f '..
-                tostring(CFLAGS)..
-                ' -o %o',
-        outputs=obj
-    }
-    OBJECTS += obj
+
+
+
+function compile(compiler, flags, sources)
+    objects = {}
+    for _,src in pairs(sources) do
+        obj="build/"..src:gsub("/","_")..".o"
+        tup.frule{
+            inputs=src,
+            command=compiler..' -c %f '..
+                    tostring(flags)..
+                    ' -o %o',
+            outputs=obj
+        }
+        objects += obj
+    end
+    return objects
 end
 
+c_obj = compile('arm-linux-gnueabihf-gcc', CFLAGS, C_SOURCES)
+cpp_obj = compile('arm-linux-gnueabihf-g++ -std=c++11', CFLAGS, CPP_SOURCES)
+
+
+objects = {}
+for _,obj in pairs(c_obj) do objects += obj end
+for _,obj in pairs(cpp_obj) do objects += obj end
+
 tup.frule{
-    inputs=OBJECTS,
+    inputs=objects,
     command='arm-linux-gnueabihf-g++ %f '..
             tostring(CFLAGS)..' '..
             tostring(LDFLAGS)..
             ' -o %o',
     outputs='build/lightd'
 }
-
