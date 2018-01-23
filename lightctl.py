e31270be4b148d296bba0ec365a317042fe55bc6
Samuel Sadok <samuel.sadok@bluewin.ch>
2018-01-23 23:35:27 +0100

    minor python util fixes

--- lightctl.py
+++ lightctl.py
@@ -6,7 +6,7 @@ import argparse
 import sys
 import os
 
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + "/fibre/python")
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + "/fibre/python")
 import fibre
   
 
@@ -18,11 +18,11 @@ parser.add_argument("--host", metavar="HOSTNAME", action="store",
                     help="Specifies the host or IP address of the light controller.")
 parser.add_argument("color", metavar="WWRRGGBB", type=str, nargs=1,
                     help="Specifies the color code.")
-parser.add_argument("duration", metavar="SECONDS", type=float, nargs='?',
+parser.add_argument("-t", "--time", metavar="SECONDS", type=float,
                     help="Fade duration. Defaults to 0.")
 parser.add_argument("-l", "--limit-brightness", action="store_true",
                     help="don't increase brightness")
-parser.set_defaults(host="192.168.178.36", duration=0)
+parser.set_defaults(host="192.168.178.36", time=0)
 args = parser.parse_args()
 
 try:
@@ -44,5 +44,5 @@ lightcontroller.set_color(float((color >> 24) & 0xff) / 255,
                           float((color >> 16) & 0xff) / 255,
                           float((color >> 8) & 0xff) / 255,
                           float((color >> 0) & 0xff) / 255,
-                          args.duration,
+                          args.time,
                           1 if args.limit_brightness else 0)
