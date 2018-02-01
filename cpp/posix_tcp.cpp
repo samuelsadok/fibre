6d19cc2b20483c85f004097c465914afa0cf2639
Max Biegert <max@k1te.be>
2018-02-01 23:27:06 +0100

    target comments by @samuelsadok
     - add missing vector include
     - remove debug output
     - enable IPv6 support

--- cpp/posix_tcp.cpp
+++ cpp/posix_tcp.cpp
@@ -6,20 +6,21 @@
 #include <unistd.h>
 #include <thread>
 #include <future>
+#include <vector>
 
 #include <fibre/protocol.hpp>
 
 
 #define TCP_RX_BUF_LEN	512
 
-class TCPBytesSender : public StreamSink {
+class TCPStreamSink : public StreamSink {
 public:
-    TCPBytesSender(int socket_fd) :
-        _socket_fd(socket_fd)
+    TCPStreamSink(int socket_fd) :
+        socket_fd_(socket_fd)
     {}
 
     int process_bytes(const uint8_t* buffer, size_t length, size_t& processed_bytes) {
-        int bytes_sent = send(_socket_fd, buffer, length, 0);
+        int bytes_sent = send(socket_fd_, buffer, length, 0);
         processed_bytes = (bytes_sent == -1) ? 0 : bytes_sent;
         return (bytes_sent == -1) ? -1 : 0;
     }
@@ -27,16 +28,15 @@ public:
     size_t get_free_space() { return SIZE_MAX; }
 
 private:
-    int _socket_fd;
+    int socket_fd_;
 };
 
 
 int serve_client(const Endpoint endpoints[], size_t n_endpoints, int sock_fd) {
     uint8_t buf[TCP_RX_BUF_LEN];
 
-//    printf("initializing output stack\n");
     // initialize output stack for this client
-    TCPBytesSender tcp_packet_output(sock_fd);
+    TCPStreamSink tcp_packet_output(sock_fd);
     StreamBasedPacketSink packet2stream(tcp_packet_output);
     BidirectionalPacketBasedChannel channel(endpoints, n_endpoints, packet2stream);
 
@@ -47,13 +47,12 @@ int serve_client(const Endpoint endpoints[], size_t n_endpoints, int sock_fd) {
         memset(buf, 0, sizeof(buf));
         // returns as soon as there is some data
         ssize_t n_received = recv(sock_fd, buf, sizeof(buf), 0);
-        printf("recvd something: %d\n", n_received);
-        if (n_received == -1 || n_received == 0) {// -1 indicates error and 0 means that the client gracefully terminated
+
+        // -1 indicates error and 0 means that the client gracefully terminated
+        if (n_received == -1 || n_received == 0) {
             close(sock_fd);
             return n_received;
         }
-//        printf("Received packet from %s:%d\n\n",
-//            inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
 
         // input processing stack
         size_t processed = 0;
@@ -68,19 +67,22 @@ bool future_is_ready(std::future<T>& t){
 }
 
 int serve_on_tcp(const Endpoint endpoints[], size_t n_endpoints, unsigned int port) {
-    struct sockaddr_in si_me, si_other;
+    struct sockaddr_in6 si_me, si_other;
     int s;
 
 
-    if ((s=socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1)
+    if ((s=socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP)) == -1) {
         return -1;
+    }
 
     memset((char *) &si_me, 0, sizeof(si_me));
-    si_me.sin_family = AF_INET;
-    si_me.sin_port = htons(port);
-    si_me.sin_addr.s_addr = htonl(INADDR_ANY);
-    if (bind(s, reinterpret_cast<struct sockaddr *>(&si_me), sizeof(si_me)) == -1)
+    si_me.sin6_family = AF_INET6;
+    si_me.sin6_port = htons(port);
+    si_me.sin6_flowinfo = 0;
+    si_me.sin6_addr = in6addr_any;
+    if (bind(s, reinterpret_cast<struct sockaddr *>(&si_me), sizeof(si_me)) == -1) {
         return -1;
+    }
 
     listen(s, 128); // make this socket a passive socket
     std::vector<std::future<int>> serv_pool;
@@ -88,14 +90,12 @@ int serve_on_tcp(const Endpoint endpoints[], size_t n_endpoints, unsigned int po
         memset(&si_other, 0, sizeof(si_other));
 
         socklen_t silen = sizeof(si_other);
-        printf("enter blocking accept\n");
+        // TODO: Add a limit on accepting connections
         int client_portal_fd = accept(s, reinterpret_cast<sockaddr *>(&si_other), &silen); // blocking call
-        printf("accepted client\n");
         serv_pool.push_back(std::async(std::launch::async, serve_client, endpoints, n_endpoints, client_portal_fd));
         // do a little clean up on the pool
         for (std::vector<std::future<int>>::iterator it = serv_pool.end()-1; it >= serv_pool.begin(); --it) {
             if (future_is_ready(*it)) {
-                printf("erasing thread\n");
                 // we can erase this thread
                 serv_pool.erase(it);
             }
