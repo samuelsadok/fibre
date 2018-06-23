1ada89779fbabb2158a25412c672dc051ad52177
Samuel Sadok <samuel.sadok@bluewin.ch>
2018-06-23 22:57:46 +0200

    TCP: keep alive

--- python/fibre/tcp_transport.py
+++ python/fibre/tcp_transport.py
@@ -1,13 +1,43 @@
 
 import sys
 import socket
+import platform
 import time
 import traceback
 import fibre.protocol
 from fibre.utils import wait_any
 
-def noprint(x):
-  pass
+# Keepalive from https://stackoverflow.com/questions/12248132/how-to-change-tcp-keepalive-timer-using-python-script
+def set_keepalive_linux(sock, after_idle_sec, interval_sec, max_fails):
+    """Set TCP keepalive on an open socket.
+
+    It activates after 1 second (after_idle_sec) of idleness,
+    then sends a keepalive ping once every 3 seconds (interval_sec),
+    and closes the connection after 5 failed ping (max_fails), or 15 seconds
+    """
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, after_idle_sec)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, interval_sec)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, max_fails)
+def set_keepalive_osx(sock, after_idle_sec, interval_sec, max_fails):
+    """Set TCP keepalive on an open socket.
+
+    sends a keepalive ping once every 3 seconds (interval_sec)
+    """
+    # scraped from /usr/include, not exported by python's socket module
+    TCP_KEEPALIVE = 0x10
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+    sock.setsockopt(socket.IPPROTO_TCP, TCP_KEEPALIVE, interval_sec)
+def set_keepalive_windows(sock, after_idle_sec, interval_sec, max_fails):
+    sock.ioctl(socket.SIO_KEEPALIVE_VALS, (1, after_idle_sec*1000, interval_sec*1000))
+def set_keepalive(sock, after_idle_sec=30, interval_sec=10, max_fails=5):
+  if platform.system() == 'Windows':
+    set_keepalive_windows(sock, after_idle_sec, interval_sec, max_fails)
+  elif platform.system() == 'Darwin':
+    set_keepalive_osx(sock, after_idle_sec, interval_sec, max_fails)
+  else:
+    set_keepalive_linux(sock, after_idle_sec, interval_sec, max_fails)
+
 
 class TCPTransport(fibre.protocol.StreamSource, fibre.protocol.StreamSink):
   def __init__(self, dest_addr, dest_port, logger):
@@ -21,6 +51,7 @@ class TCPTransport(fibre.protocol.StreamSource, fibre.protocol.StreamSink):
     self.target = socket.getaddrinfo(dest_addr, dest_port, family)[0][4]
     # TODO: this blocks until a connection is established, or the system cancels it
     self.sock.connect(self.target)
+    set_keepalive(self.sock)
 
   def process_bytes(self, buffer):
     self.sock.send(buffer)
@@ -33,18 +64,19 @@ class TCPTransport(fibre.protocol.StreamSource, fibre.protocol.StreamSink):
     to non-blocking mode.
     """
     # convert deadline to seconds (floating point)
-    deadline = None if deadline is None else max(deadline - time.monotonic(), 0)
-    self.sock.settimeout(deadline)
+    timeout = None if deadline is None else max(deadline - time.monotonic(), 0)
+    self.sock.settimeout(timeout)
     try:
       data = self.sock.recv(n_bytes, socket.MSG_WAITALL) # receive n_bytes
       return data
     except socket.timeout:
       # if we got a timeout data will still be none, so we call recv again
       # this time in non blocking state and see if we can get some data
-      try:
-        return self.sock.recv(n_bytes, socket.MSG_DONTWAIT)
-      except socket.timeout:
-        raise TimeoutError
+      #return self.sock.recv(n_bytes, socket.MSG_DONTWAIT)
+      #try:
+      #  return self.sock.recv(n_bytes, socket.MSG_DONTWAIT)
+      #except socket.timeout:
+      raise TimeoutError
 
   def get_bytes_or_fail(self, n_bytes, deadline):
     result = self.get_bytes(n_bytes, deadline)
