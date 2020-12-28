06ca512672990e657760ece0859430473fb7fad3
Samuel Sadok <samuel.sadok@bluewin.ch>
2020-12-28 11:28:20 +0100

    initial formatting checks

--- test/test_server.cpp
+++ test/test_server.cpp
@@ -1,17 +1,16 @@
 
+#include "autogen/interfaces.hpp"  // TODO: remove this include
+#include <fibre/fibre.hpp>
+#include <iostream>
 #include <stdio.h>
 #include <unistd.h>
-#include <iostream>
-
-#include <fibre/fibre.hpp>
-
-#include "autogen/interfaces.hpp" // TODO: remove this include
 
 class TestClass : public TestIntf1Intf {
 public:
     void func00() {
         std::cout << "func00 called" << std::endl;
     }
+
     uint32_t func01() {
         std::cout << "func01 called" << std::endl;
         return 123;
@@ -26,9 +25,7 @@ int main() {
     bool ok = fibre::launch_event_loop([](fibre::EventLoop* event_loop) {
         printf("Hello from event loop...\n");
         auto fibre_ctx = fibre::open(event_loop);
-        //fibre_ctx->create_domain("tcp-client:address=innovation-labs.appinstall.ch,port=3214");
         fibre_ctx->create_domain("tcp-server:address=localhost,port=14220");
-        //fibre::publish(test_object, "tcp-server:address=localhost,port=14220");
     });
 
     printf("test server terminated %s\n", ok ? "nominally" : "with an error");
@@ -36,9 +33,8 @@ int main() {
     return ok ? 0 : 1;
 }
 
-
+#include "autogen/function_stubs.hpp"
 #include <fibre/../../crc.hpp>
 #include <fibre/../../protocol.hpp>
-#include "autogen/function_stubs.hpp"
 TestClass& ep_root = test_object;
 #include "autogen/endpoints.hpp"
